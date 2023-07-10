f32a2ab0435ca015126fc305c242e4ec7cbb8ae3
Himanshu <thimanshu937@gmail.com>
2023-07-10 23:29:40 +0530

    commit version 2.0.0  NOW IMAGE VIEW IS ADDED TO SYSTEM , USER IS NOW SHOWN IMAGE OF THE RESULT NOT THE ACTUAL NUMBER.
    for older version 1.0.0 :-
     Here user can roll the dice and can take info of how many time the dice has already been rolled.

--- app/src/main/java/com/supercruze/diceroller/MainActivity.kt
+++ app/src/main/java/com/supercruze/diceroller/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.supercruze.diceroller
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,26 +12,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val txtView : TextView = findViewById(R.id.DiceRollResultTxtView)
-        val rollBtn : Button =findViewById(R.id.DiceRollBtn)
-         
+        val diceImageView: ImageView = findViewById(R.id.diceImageView)
+        val rollBtn: Button = findViewById(R.id.DiceRollBtn)
+
         val diceObj = Dice(6)
-        var numOfClickes : Int = 0
+        var numOfClickes = 0
+        var rollResult: Int
         rollBtn.setOnClickListener {
 
-            txtView.text = diceObj.rollDice()
+            rollResult = diceObj.rollDice()
             numOfClickes++
-            Toast.makeText(this,"Dice Rolled "+numOfClickes+" Times",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Dice Rolled " + numOfClickes + " Times", Toast.LENGTH_SHORT)
+                .show()
+            when (rollResult) {
+
+                1 -> diceImageView.setImageResource(R.drawable.dice_1)
+                2 -> diceImageView.setImageResource(R.drawable.dice_2)
+                3 -> diceImageView.setImageResource(R.drawable.dice_3)
+                4 -> diceImageView.setImageResource(R.drawable.dice_4)
+                5 -> diceImageView.setImageResource(R.drawable.dice_5)
+                6 -> diceImageView.setImageResource(R.drawable.dice_6)
+            }
+            diceImageView.contentDescription = rollResult.toString()
         }
     }
 }
 
-class Dice(private val numOfSides : Int)  {
+class Dice(numOfSides: Int) {
 
     val diceRange = 1..numOfSides
 
-    
-    fun rollDice() : String {
-       return diceRange.random().toString()
+
+    fun rollDice(): Int {
+        return diceRange.random()
     }
 }
